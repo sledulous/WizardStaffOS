8acc0be4ffc04caa91d658336b54a7cc065fea90
sledulous <45291948+sledulous@users.noreply.github.com>
2021-06-07 21:45:45 -0400

    Add files via upload

---
+++ WizardStaffOS.ino
@@ -0,0 +1,80 @@
+#include <Adafruit_NeoPixel.h>
+
+#define BUTTON_PIN   1    // Digital IO pin connected to the button.  This will be
+                          // driven with a pull-up resistor so the switch should
+                          // pull the pin to ground momentarily.  On a high -> low
+                          // transition the button press logic will execute.
+
+#define PIXEL_PIN    10   // Digital IO pin connected to the NeoPixels.
+
+#define PIXEL_COUNT 1   // number of neopixel (change this accordingly)
+
+// Parameter 1 = number of pixels in strip,  neopixel stick has 8
+// Parameter 2 = pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_RGB     Pixels are wired for RGB bitstream
+//   NEO_GRB     Pixels are wired for GRB bitstream, correct for neopixel stick
+//   NEO_KHZ400  400 KHz bitstream (e.g. FLORA pixels)
+//   NEO_KHZ800  800 KHz bitstream (e.g. High Density LED strip), correct for neopixel stick
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
+
+bool oldState = HIGH;
+int showType = 0;
+
+void setup() {
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+}
+
+void loop() {
+  // Get current button state.
+  bool newState = digitalRead(BUTTON_PIN);
+
+  // Check if state changed from high to low (button press).
+  if (newState == LOW && oldState == HIGH) {
+    // Short delay to debounce button.
+    delay(20);
+    // Check if button is still low after debounce.
+    newState = digitalRead(BUTTON_PIN);
+    if (newState == LOW) {
+      showType++;
+      if (showType > 7)
+        showType=0;
+      startShow(showType);
+    }
+  }
+
+  // Set the last button state to the old state.
+  oldState = newState;
+}
+
+void startShow(int i) {
+  switch(i){
+    case 0: colorWipe(strip.Color(0, 0, 0), 50);    // Black/off
+            break;
+    case 1: colorWipe(strip.Color(255, 0, 0), 50);  // Red
+            break;
+    case 2: colorWipe(strip.Color(0, 255, 0), 50);  // Green
+            break;
+    case 3: colorWipe(strip.Color(0, 0, 255), 50);  // Blue
+            break;
+    case 4: colorWipe(strip.Color(255, 100, 0), 50);  // Orange
+            break;
+    case 5: colorWipe(strip.Color(255, 175, 0), 50);  // Yellow
+            break;
+    case 6: colorWipe(strip.Color(255, 0, 175), 50);  // Purple
+            break;
+    case 7: colorWipe(strip.Color(255, 255, 255), 50);  // White
+            break;
+  }
+}
+
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
+  }
+}
